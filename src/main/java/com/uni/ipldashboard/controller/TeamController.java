f8c6608c6101083d26be6b48e6c34f441a1682cc
Kishore V <kishorevjn835@gmail.com>
2021-08-09 20:40:07 +0530

    match api changes

--- src/main/java/com/uni/ipldashboard/controller/TeamController.java
+++ src/main/java/com/uni/ipldashboard/controller/TeamController.java
@@ -1,5 +1,8 @@
 package com.uni.ipldashboard.controller;
 
+import java.util.List;
+
+import com.uni.ipldashboard.model.Match;
 import com.uni.ipldashboard.model.Team;
 import com.uni.ipldashboard.service.AppService;
 
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,11 @@ public class TeamController {
         Team team = this.appService.getTeam(teamName, 4);
         return team;
 
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+
+        return this.appService.getMatchesForTeam(teamName, year);
     }
 }
--- src/main/java/com/uni/ipldashboard/service/AppService.java
+++ src/main/java/com/uni/ipldashboard/service/AppService.java
@@ -1,5 +1,9 @@
 package com.uni.ipldashboard.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.uni.ipldashboard.model.Match;
 import com.uni.ipldashboard.model.Team;
 import com.uni.ipldashboard.repository.MatchRepository;
 import com.uni.ipldashboard.repository.TeamRepository;
@@ -10,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AppService {
-    
+
     TeamRepository teamRepository;
     MatchRepository matchRepository;
 
@@ -21,9 +25,16 @@ public class AppService {
 
     public Team getTeam(String teamName, int count) {
 
-        Team team =  this.teamRepository.findByTeamName(teamName);
+        Team team = this.teamRepository.findByTeamName(teamName);
         Pageable pageable = PageRequest.of(0, count);
         team.setMatches(matchRepository.findByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable));
         return team;
+    }
+
+    public List<Match> getMatchesForTeam(String teamName, int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return matchRepository.getMatchesByNameandDate(teamName, startDate, endDate);
+
     }
 }
